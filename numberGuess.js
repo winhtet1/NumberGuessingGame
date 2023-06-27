4c3b25c8904a334db327955130668233c3d7c37b
Win Htet Aung <51900376+winhtet1@users.noreply.github.com>
2023-06-27 23:09:04 +0630

    Create numberGuess.js

---
+++ numberGuess.js
@@ -0,0 +1,16 @@
+const answer = Math.floor(Math.random()* 10 + 1) ;
+let guesses = 0;
+
+document.getElementById("submitBtn").onclick = function(){
+
+    let guess = document.getElementById("guessField").value
+    console.log(guess);
+    guesses += 1;
+    if(answer == guess){
+        alert(`Congratulation!! ${guess} is a number. You took ${guesses} times.`);
+    } else if (answer < guess){
+        alert("Too Large!!");
+    } else {
+        alert("Too Small!!");
+    }
+}
